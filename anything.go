76aeded9e97c22fd3b5456e3ee026220aa7799ac
Jeewan Rana <jeewanrana@sv-lt-1670.lan>
2020-10-14 10:03:30 -0700

    adds fav quotes

---
+++ anything.go
@@ -0,0 +1,16 @@
+package quotes
+
+func Favs() []string {
+	return []string{
+		"Don’t grieve. Anything you lose comes round in another form.",
+		"Stop acting so small. You are the universe in ecstatic motion.",
+		"Out beyond ideas of wrongdoing and rightdoing there is a field. I'll meet you there. When the soul lies down in that grass the world is too full to talk about.",
+		"Be melting snow. Wash yourself of yourself.",
+		"Let the waters settle and you will see the moon and the stars mirrored in your own being.",
+		"The wound is the place where the Light enters you.",
+		"Only from the heart can you touch the sky.",
+		"Lovers don't finally meet somewhere. They're in each other all along.",
+		"Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.",
+		"Sell your cleverness and buy bewilderment.",
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jerana/quotes
+
+go 1.15
